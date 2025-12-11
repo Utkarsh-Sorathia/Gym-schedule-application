23128545525147d20023956dcbf723c9e42c65ef
Utkarsh Sorathia <83358150+Utkarsh-Sorathia@users.noreply.github.com>
2025-12-11 07:03:28 +0000

    Remove PWA support and associated assets from the project

--- next.config.ts
+++ next.config.ts
@@ -1,29 +1,4 @@
 import type { NextConfig } from "next";
-import withPWAInit from "@ducanh2912/next-pwa";
-
-const withPWA = withPWAInit({
-  dest: "public",
-  cacheOnFrontEndNav: true,
-  aggressiveFrontEndNavCaching: true,
-  reloadOnOnline: true,
-  disable: process.env.NODE_ENV === "development",
-  workboxOptions: {
-    disableDevLogs: true,
-    runtimeCaching: [
-      {
-        urlPattern: /\/api\/(schedule|notes)/,
-        handler: "StaleWhileRevalidate",
-        options: {
-          cacheName: "api-cache",
-          expiration: {
-            maxEntries: 50,
-            maxAgeSeconds: 60 * 60 * 24, // 1 day
-          },
-        },
-      },
-    ],
-  },
-});
 
 const nextConfig: NextConfig = {
   /* config options here */
@@ -31,4 +6,4 @@ const nextConfig: NextConfig = {
   allowedDevOrigins: ["192.168.1.34:3000", "localhost:3000"],
 };
 
-export default withPWA(nextConfig);
+export default nextConfig;
