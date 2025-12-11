f57c5ab088cd0541cead79e8abe0a3aa1e28fbe2
Utkarsh Sorathia <83358150+Utkarsh-Sorathia@users.noreply.github.com>
2025-12-11 14:18:18 +0530

    Remove metadata and viewport from layout.tsx
    Removed metadata and viewport exports from layout.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,27 +1,9 @@
-import type { Metadata, Viewport } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
-  title: "gym-schedule-application",
-  description: "A simple app to manage your notes and tasks",
-  manifest: "/manifest.json",
-  appleWebApp: {
-    capable: true,
-    statusBarStyle: "default",
-    title: "gym-schedule-application",
-  },
-  formatDetection: {
-    telephone: false,
-  },
-};
-
-export const viewport: Viewport = {
-  themeColor: "#000000",
-};
 
 export default function RootLayout({
   children,
