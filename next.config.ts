5b7363ca1f1f8dae5609c82d18051b7562be3e0a
Utkarsh Sorathia <83358150+Utkarsh-Sorathia@users.noreply.github.com>
2025-12-11 07:30:31 +0000

    Refactor configuration files: remove unused options and format for consistency

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  reactCompiler: false,
-  allowedDevOrigins: ["192.168.1.34:3000", "localhost:3000"],
 };
 
 export default nextConfig;
